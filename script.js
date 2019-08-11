35d471870c4b217dcd929400fcaa8e306f10aa6e
BestDeadman <RuslanSalikhov@MacBook-Pro-Ruslan.local>
2019-08-11 18:26:30 +0300

    Firefox theme hotfix

--- script.js
+++ script.js
@@ -1,17 +1,17 @@
 var dayTime = false;
-function switchTheme(){
+function switchTheme() {
   if (dayTime === true) {
     /* Включаем светлую тему*/
     header.style.backgroundColor = "#22262B";
     footer.style.backgroundColor = "#22262B";
-    content.style.backgroundColor = "#292C34";
-    content.style.color = "#F5F6F7";
+    contentBlock.style.backgroundColor = "#292C34";
+    contentBlock.style.color = "#F5F6F7";
   } else {
     /* Включаем темную тему*/
     header.style.backgroundColor = "#5582AB";
     footer.style.backgroundColor = "#5582AB";
-    content.style.backgroundColor = "#F5F6F7";
-    content.style.color = "#292C34";
+    contentBlock.style.backgroundColor = "#F5F6F7";
+    contentBlock.style.color = "#292C34";
   }
   dayTime =!dayTime;
 }
