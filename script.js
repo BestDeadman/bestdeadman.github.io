998f8f4cd247a84d7055f04121dc70a06a7f8686
BestDeadman <RuslanSalikhov@MacBook-Pro-Ruslan.local>
2019-09-04 06:45:10 +0300

    Logo and Phone-design test

--- script.js
+++ script.js
@@ -1,15 +1,16 @@
+jQuery(document).ready(function(){
+  //var newPost = $("<div></div>");
+  //$("#contentBlock").prepend("#newPost");
+});
+
 var dayTime = false;
 function switchTheme() {
   if (dayTime === true) {
-    /* Включаем светлую тему*/
-    header.style.backgroundColor = "#22262B";
-    footer.style.backgroundColor = "#22262B";
+    // Включаем светлую тему
     contentBlock.style.backgroundColor = "#292C34";
     contentBlock.style.color = "#F5F6F7";
   } else {
-    /* Включаем темную тему*/
-    header.style.backgroundColor = "#5582AB";
-    footer.style.backgroundColor = "#5582AB";
+    // Включаем темную тему*/
     contentBlock.style.backgroundColor = "#F5F6F7";
     contentBlock.style.color = "#292C34";
   }
